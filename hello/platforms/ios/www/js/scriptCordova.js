8b70b0993265851ff0310e28bf459791a58fe0b9
xazer18 <lucas.desmedt62@gmail.com>
2018-03-23 12:52:54 +0100

    update

--- hello/platforms/ios/www/js/scriptCordova.js
+++ hello/platforms/ios/www/js/scriptCordova.js
@@ -1,6 +1,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
+  var name = device.name;
   var model = device.model;
   var platform = device.platform;
   var version = device.version;
@@ -8,7 +9,24 @@ function onDeviceReady() {
 
   alert("Modèle: "+model+"\nPlateforme: "+platform+"\nVersion: "+version+"\nUUID: "+uuid);
 
-  // StatusBar.show();
-  // StatusBar.backgroundColorByHexString("#FB6400");
+  StatusBar.backgroundColorByHexString("#FB6400");
+
+
+//   function onSuccess(contacts) {
+//     alert('Found ' + contacts.length + ' contacts.');
+//   };
+//
+// function onError(contactError) {
+//     alert('onError!');
+//   };
+
+  // contacts
+  // var options = new ContactFindOptions();
+  // options.filter = *;
+  // options.multiple = true;
+  // options.desiredFields = [navigator.contacts.fieldType.id];
+  // options.hasPhoneNumber = true;
+  // var field = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+  // navigator.contacts.find(fields, onSuccess, onError, options);
 
 }
--- hello/www/js/scriptCordova.js
+++ hello/www/js/scriptCordova.js
@@ -1,11 +1,32 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
+  var name = device.name;
   var model = device.model;
   var platform = device.platform;
   var version = device.version;
   var uuid = device.uuid;
 
   alert("Modèle: "+model+"\nPlateforme: "+platform+"\nVersion: "+version+"\nUUID: "+uuid);
+
+  StatusBar.backgroundColorByHexString("#FB6400");
+
+
+//   function onSuccess(contacts) {
+//     alert('Found ' + contacts.length + ' contacts.');
+//   };
+//
+// function onError(contactError) {
+//     alert('onError!');
+//   };
+
+  // contacts
+  // var options = new ContactFindOptions();
+  // options.filter = *;
+  // options.multiple = true;
+  // options.desiredFields = [navigator.contacts.fieldType.id];
+  // options.hasPhoneNumber = true;
+  // var field = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+  // navigator.contacts.find(fields, onSuccess, onError, options);
 
 }
